bbe983ea44aea86db60b572b33cf57f992dac841
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-02 19:56:03 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,6 +1,10 @@
 var index = require("./index.js")
+var other_file = require("./other_file.js")
 
-if (index.name === "Susan") {
+console.log(index.name);
+console.log(index.height);
+
+if (index.name === "Joe") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -8,7 +12,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
