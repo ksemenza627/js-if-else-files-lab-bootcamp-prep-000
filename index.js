f2a42f70d1027b5975708e175fd92320e3f9da70
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-02 19:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,5 @@ var height = "74"
 
 
 // Don't worry about this
-module.exports = { name, height}
-
-if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
-}
-
-if (height === "70"){
-  console.log("The height is correct")
-} else {
-  console.console.log("Expected the height to be 70");
+module.exports = { name, height
 }
