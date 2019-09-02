506d000387eff08e1daa34312d30ab72239ebcc2
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-02 19:28:50 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,7 +1,3 @@
 var index = require("./index.js")
 
-if (name === "Susan") {
-  console.log("Name is Correct")
-} else {
-  console.console.log("Expected: Susan Received: " index.name);
-}
+console.log(index.name);
